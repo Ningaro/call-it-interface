ab4c92fb9e8c09f832b7e4f4c623639a8ddd6390
iuriinesterov <55027018+iuriinesterov@users.noreply.github.com>
2019-10-10 18:19:05 +0300

    some thing new

--- js/customJS.js
+++ js/customJS.js
@@ -1,3 +1,10 @@
+function workSpace() {
+  $("#botaBegin").html(`Готово`);
+  $(".container").css("opacity", "0");
+  $("body").css({"display": "flex",
+  "align-items": "center"});
+  $("body").html();
+}
 $("#logBtn").on("click", function() {
   if (($("#login").val() == "") && ($("#password").val() == "")) {
     console.log('123');
@@ -11,4 +18,5 @@ $("#botaBegin").on("click", function() {
   $("#botaBegin").prop('disabled', true);
   $("#botaBegin").html(`<span class="spinner-grow" role="status" aria-hidden="true" style="width: 1.5rem;height: 1.5rem;"></span>
   Загрузка...`);
+  setTimeout(workSpace, 3000);
 });
